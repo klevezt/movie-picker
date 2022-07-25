b28e83e40fcfab1bd1dec4a8c0d56c627316ca45
klevezt <klevestpalucaj@gmail.com>
2022-07-25 15:28:28 +0300

    changes

--- src/components/Content/Main/Main.js
+++ src/components/Content/Main/Main.js
@@ -3,6 +3,7 @@ import { truncateString } from "../../Helpers/functions";
 import Pagination from "../../UI/Pagination/Pagination";
 import SkeletonWithImage from "../../UI/Skeleton/SkeletonWithImage";
 import styles from "./Main.module.css";
+import { FavoriteBorder, Home } from "@mui/icons-material";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
@@ -20,29 +21,30 @@ const Main = () => {
       console.log(result.results);
       setMovies(result.results);
       setTimeout(() => {
-        
-          setIsLoading(false);
-      }, 2000);
+        setIsLoading(false);
+      }, 1000);
     };
     exec();
   }, [page]);
 
   const allMovies = movies.map((movie, i) => {
-    return isLoading ? (
-      <SkeletonWithImage key={i} />
-    ) : (
-      <div className="col-6" key={i}>
-        <div className={styles["row-wrapper"]}>
-          <img
-            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            alt={`movie-${movie.title}`}
-          />
-          <div className={styles["row-content"]}>
-            <h2>{movie.title}</h2>
-            <h4>Release date: {movie.release_date}</h4>
-            <p>{truncateString(movie.overview, 200)}</p>
+    return (
+      <div className="col-12" key={i}>
+        {isLoading ? (
+          <SkeletonWithImage key={i} />
+        ) : (
+          <div className={styles["row-wrapper"]}>
+            <img
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              alt={`movie-${movie.title}`}
+            />
+            <div className={styles["row-content"]}>
+              <h2>{movie.title}</h2>
+              <h4>Release date: {movie.release_date}</h4>
+              <p>{truncateString(movie.overview, 200)}</p>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   });
@@ -54,9 +56,38 @@ const Main = () => {
   return (
     <Fragment>
       <div className="container">
-        <div className="row">{allMovies}</div>
+        <div className="row">
+          <div className="col-3 row">
+            <nav
+              id="sidebarMenu"
+              className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+            >
+              <div className="pt-3 sidebar-sticky">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <div className="nav-link active" aria-current="page">
+                      <Home className="me-2" />
+                      Home
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <div className="nav-link">Orders</div>
+                  </li>
+                  <li className="nav-item">
+                    <div className="nav-link">
+                      <FavoriteBorder className="me-2" />
+                      Favorite
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+          <div className="col-7 row">{allMovies}</div>
+          <div className="col-2">RIGHT</div>
+        </div>
+        <Pagination current={page} onClickNext={onClickNext} />
       </div>
-      <Pagination current={page} onClickNext={onClickNext} />
     </Fragment>
   );
 };
